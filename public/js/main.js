bc28374be0dac8b5ef203f183f4ba4e81b78b50e
Helena Spencer <helenaspencer@macbook-pro-3.myfiosgateway.com>
2022-04-16 23:36:33 -0400

    basic layout of home page

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    nav_autohide = document.querySelector('.autohideNav');
+    ham_autohide = document.querySelector('.autohideHam');
+    
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+  
+    if(nav_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < 50) {
+                nav_autohide.classList.add('scrolled-down');
+                ham_autohide.classList.remove('scrolled-down');
+            }
+            else {
+                nav_autohide.classList.remove('scrolled-down');
+                ham_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+      // window.addEventListener
+    }
+    // if
+  
+}); 
